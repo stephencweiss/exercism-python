e3b1a312c2d1259715081cd6794bd49dcb8c2666
Stephen <stephen.c.weiss@gmail.com>
2020-05-13 20:39:08 -0500

    refactor: generators!

--- armstrong-numbers/armstrong_numbers.py
+++ armstrong-numbers/armstrong_numbers.py
@@ -2,5 +2,4 @@ import functools
 
 
 def is_armstrong_number(number):
-    digits = [pow(int(digit), len(str(number))) for digit in str(number)]
-    return number == functools.reduce(lambda a, b: a+b, digits)
+    return sum(pow(int(digit), len(str(number))) for digit in str(number)) == number
