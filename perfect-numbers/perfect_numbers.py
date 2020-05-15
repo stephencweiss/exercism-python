7d40cc6ff66cbcc18c7f868a755c872569feca83
Stephen <stephen.c.weiss@gmail.com>
2020-05-15 08:43:06 -0500

    fix: avoid list by using generator
    replacing the list comprehension and going directly to the desired out-
    come: the aliquot sum is slightly more efficient.

--- perfect-numbers/perfect_numbers.py
+++ perfect-numbers/perfect_numbers.py
@@ -1,13 +1,14 @@
 def classify(number):
     if number <= 0:
         raise ValueError("Classification works only for positive integers")
-    if sum(factors_of_n(number)) == number:
+    aliquot_sum = find_aliquot_sum(number)
+    if aliquot_sum == number:
         return "perfect"
-    elif sum(factors_of_n(number)) < number:
+    elif aliquot_sum < number:
         return "deficient"
-    elif sum(factors_of_n(number)) > number:
+    elif aliquot_sum > number:
         return "abundant"
 
 
-def factors_of_n(number):
-    return [factor for factor in range(1, int(number/2)+1) if number % factor == 0]
+def find_aliquot_sum(number):
+    return sum(factor for factor in range(1, int(number/2)+1) if number % factor == 0)
